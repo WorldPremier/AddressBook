752a709ba9a2f71de2ca0632a1ba2149f26325cd
Franklyn Reyes <franklynreyes777@yahoo.com>
2021-05-10 08:40:19 -0400

    Initial commit

---
+++ src/main/java/com/tts/AddressBook.java
@@ -0,0 +1,12 @@
+package com.tts;
+import java.util.*;
+
+
+public class AddressBook {
+    Scanner sc = new Scanner(System.in);
+    String fName = sc.nextLine();
+    String lName =  sc.nextLine();
+    String email  = sc.nextLine();
+    String phoneNum = sc.nextLine();
+
+}
---
+++ src/main/java/com/tts/Main.java
@@ -0,0 +1,31 @@
+package com.tts;
+import org.w3c.dom.ls.LSOutput;
+
+
+import java.util.*;
+
+public class Main<Int> {
+
+
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("1) Add an entry\n" +
+                "2) Remove an entry\n" +
+                "3) Search for a specific entry\n" +
+                "4) Print Address Book\n" +
+                "5) Delete Book\n" +
+                "6) Quit\n" +
+                "Please choose what you'd like to do with the database:\n");
+        int entryInput = sc.nextInt();
+
+
+
+
+
+
+    }
+
+}
+
